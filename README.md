ac413c2d4e51963531f67683bfed7d1f3fc8b8f7
Purushotham <xcode.ypr@gmail.com>
2017-08-31 10:58:03 +0530

    landing Page
    landing Page

--- README.md
+++
@@ -1,2 +0,0 @@
-# Dookomi
-Classified app
---
+++ js/scripts.js
@@ -0,0 +1,128 @@
+var appMaster = {
+
+    preLoader: function(){
+        imageSources = []
+        $('img').each(function() {
+            var sources = $(this).attr('src');
+            imageSources.push(sources);
+        });
+        if($(imageSources).load()){
+            $('.pre-loader').fadeOut('slow');
+        }
+    },
+
+    smoothScroll: function() {
+        // Smooth Scrolling
+        $('a[href*=#]:not([href=#carousel-example-generic])').click(function() {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    },
+
+    reviewsCarousel: function() {
+        // Reviews Carousel
+        $('.review-filtering').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: true,
+            arrows: false,
+            autoplay: true,
+            autoplaySpeed: 5000
+        });
+    },
+
+    screensCarousel: function() {
+        // Screens Carousel
+        $('.filtering').slick({
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            dots: false,
+			infinite:false,
+            responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2
+                }
+            }, {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2
+                }
+            }, {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1
+                }
+            }]
+        });
+
+    },
+
+    animateScript: function() {
+        $('.scrollpoint.sp-effect1').waypoint(function(){$(this).toggleClass('active');$(this).toggleClass('animated fadeInLeft');},{offset:'100%'});
+        $('.scrollpoint.sp-effect2').waypoint(function(){$(this).toggleClass('active');$(this).toggleClass('animated fadeInRight');},{offset:'100%'});
+        $('.scrollpoint.sp-effect3').waypoint(function(){$(this).toggleClass('active');$(this).toggleClass('animated fadeInDown');},{offset:'100%'});
+        $('.scrollpoint.sp-effect4').waypoint(function(){$(this).toggleClass('active');$(this).toggleClass('animated fadeIn');},{offset:'100%'});
+        $('.scrollpoint.sp-effect5').waypoint(function(){$(this).toggleClass('active');$(this).toggleClass('animated fadeInUp');},{offset:'100%'});
+    },
+
+    
+
+    Hover_active_img: function(){
+		$('#features .media').hover(function(){
+			var target = $(this).attr('rel');
+			$('#active_img').attr('src', target);
+		},function(){
+			//var target = $(this).attr('rel');
+			$('#active_img').attr('src', 'images/home.png');
+		});
+	},
+	
+	scrollMenu: function(){
+        var num = 50; //number of pixels before modifying styles
+        if ($(window).scrollTop() > num) {
+            $('nav').addClass('scrolled');
+        }
+        $(window).bind('scroll', function () {
+            if ($(window).scrollTop() > num) {
+                $('nav').addClass('scrolled');
+
+            } else {
+                $('nav').removeClass('scrolled');
+            }
+        });
+
+        $('ul.navbar-nav li a').bind('click', function(){
+            if($(this).closest('.navbar-collapse').hasClass('in')){
+                $(this).closest('.navbar-collapse').removeClass('in');
+            }
+        });
+        
+    }
+
+}; // AppMaster
+
+
+$(document).ready(function() {
+
+    appMaster.smoothScroll();
+
+    appMaster.reviewsCarousel();
+
+    appMaster.screensCarousel();
+
+    appMaster.animateScript();
+
+    appMaster.scrollMenu();
+	
+	appMaster.Hover_active_img();
+
+});
